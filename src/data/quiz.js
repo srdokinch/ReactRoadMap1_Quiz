374e4ec0d2e0003ff83ffd8b20ad2f72b1102f6b
srdokinch <srdokinch@gmail.com>
2026-04-20 00:22:45 +0900

    buttonを表示してmap()で整えた

---
+++ src/data/quiz.js
@@ -0,0 +1,29 @@
+const quizData = [
+  {
+    question: "質問1です",
+    options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+    answerIndex: 0
+  },
+  {
+    question: "質問2です",
+    options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+    answerIndex: 1
+  },
+  {
+    question: "質問3です",
+    options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+    answerIndex: 2
+  },
+  {
+    question: "質問4です",
+    options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+    answerIndex: 3
+  },
+  {
+    question: "質問5です",
+    options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+    answerIndex: 0
+  }
+]
+
+export default quizData
--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,5 +1,26 @@
+import Display from '../components/Display/Display'
+import quizData from '../data/quiz'
+import Button from '../components/Button/Button'
+
 export default function QuizPage() {
+    const quizIndex = 0
+    const handleClick = (clickedIndex) => {
+      console.log("clickedIndex", clickedIndex)
+    }
   return (
-    <div>QuizPage</div>
+    <>
+      <Display>
+        {`Q1. ${quizData[0].question}`}
+      </Display>
+      {
+        quizData[quizIndex].options.map((option, index) => {
+          return (
+            <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+              { option }
+            </Button>
+          )
+        })
+      }
+    </>
   )
 }
